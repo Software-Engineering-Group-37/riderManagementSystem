f400ffa1a9757560085ed96466f4b4248859f76e
Mulcah <emamonoo@st.ug.edu.gh>
2025-08-04 01:21:35 +0000

    route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -72,7 +72,7 @@ const App = () => {
             <Route path="/forgot-password" element={<ForgotPassword />} />
 
             {/* Fallback */}
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
+            <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
     );
 }
